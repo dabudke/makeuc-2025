fc1282782a3d288c4446d87de775f452b983a69c
SpectralGT <atharvsingh1396@gmail.com>
2025-11-08 20:51:16 -0500

    zero shot

---
+++ backend/src/zero-shot-classification.js
@@ -0,0 +1,46 @@
+async function query(data) {
+  const response = await fetch(
+    "https://router.huggingface.co/hf-inference/models/facebook/bart-large-mnli",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HF_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result;
+}
+
+const classify = async (title) => {
+  await query({
+    inputs:
+    title,
+    parameters: {
+      candidate_labels: ["Clothes", "Food", "Cosmetic", "Electronics"],
+    },
+  }).then((response) => {
+    const results = response;
+    console.log(JSON.stringify(response));
+    console.log(response);
+
+    const max = results.reduce((prev, current) =>
+      prev && prev["score"] > current["score"] ? prev : current
+    );
+    if (max["score"] > 0.5) {
+        console.log(max["label"])
+        return max["label"];
+    }
+    return "Other";
+    
+  });
+};
+
+async function test() {
+    const result = await classify("TACVASEN Men's Winter Skiing Jackets Water Resistant Fleece Coats Insulated Thermal Snowboard Jacket Parka Raincoat with Hood")
+    console.log(result)
+}
+
+test()
